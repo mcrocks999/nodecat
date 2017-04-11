f8e2d5443dea838e26953c06f55382597c70a167
mcrocks999 <paulmakles@gmail.com>
2017-04-11 09:27:03 +0100

    Script didn't upload, try 2

--- catgenerator.js
+++ catgenerator.js
@@ -15,6 +15,28 @@ function repeatCat(image,repeat,x,source,callback) {
 	});
 }
 
+function prepareCat(image,repeat,repeated,callback,flip) {
+	repeatCat(image,repeat,0,repeated,function(image) {
+		if (flip) {
+			image.flip('x', function(err, image){
+				saveCat(image,callback);
+			});
+		} else {
+			saveCat(image,callback);
+		}
+	});
+}
+
+function saveCat(image,callback) {
+	var temporary = Math.floor(Math.random()*99999999)+'.png';
+	image.writeFile(temporary, function(err){
+		callback(temporary,function(){
+			var fs = require('fs');
+			fs.unlinkSync(temporary);
+		});
+	});
+}
+
 module.exports = {
 	assets,
 	create: function(repeat,callback) {
@@ -25,21 +47,16 @@ module.exports = {
 			return;
 		}
 		var lwip = require('lwip');
+		var flip = false;
+		if (repeat<0) flip = true;
+		repeat = Math.abs(repeat);
 		lwip.open(assets.top, function(err, start){
 			lwip.open(assets.bottom, function (err, end){
 				lwip.open(assets.middle, function (err, repeated){
 					lwip.create(62+50+((repeat+1)*15), 54, [0,0,0,0], function(err, image){
 						image.paste(0, 0, end, function(err, image){
 							image.paste(62+((repeat+1)*15), 0, start, function(err, image){
-								repeatCat(image,repeat,0,repeated,function(image) {
-									var temporary = Math.floor(Math.random()*99999999)+'.png';
-									image.writeFile(temporary, function(err){
-										callback(temporary,function(){
-											var fs = require('fs');
-											fs.unlinkSync(temporary);
-										});
-									});
-								});
+								prepareCat(image,repeat,repeated,callback,flip);
 							});
 						});
 					});
